d39b7c41de44153cb5d0b83a2167bb69a8e57a46
danicc097 <danicc097@gmail.com>
2025-03-08 00:18:11 +0100

    no port in prod api frontend

--- frontend/src/services/apiPaths.ts
+++ frontend/src/services/apiPaths.ts
@@ -1,4 +1,5 @@
 export function apiPath(path?: string) {
-  const port = import.meta.env.VITE_API_PORT?.length > 0 ? `:${import.meta.env.VITE_API_PORT}` : ''
+  const port =
+    import.meta.env.DEV && import.meta.env.VITE_API_PORT?.length > 0 ? `:${import.meta.env.VITE_API_PORT}` : ''
   return `https://${import.meta.env.VITE_DOMAIN}${port}${import.meta.env.VITE_API_VERSION}${path ?? ''}`
 }
